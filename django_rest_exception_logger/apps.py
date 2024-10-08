f2fa2003a6ccbcb422cdd809814d3d2788d4c639
erenerkoc <eren.erkoc@vinya.com.tr>
2024-10-08 10:03:19 +0300

    fix - setup & apps

--- django_rest_exception_logger/apps.py
+++ django_rest_exception_logger/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoExceptionLoggerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_rest_exception_handler'
+    name = 'django_rest_exception_logger'
